2ebc8d1b1ed5f28e4664f09e7cfec5587e14bb74
becky njims <njimsbekline@gmail.com>
2025-07-31 06:06:52 +0100

    Update script.js

--- day15_coding_chalenge/script.js
+++ day15_coding_chalenge/script.js
@@ -1,6 +1,6 @@
 async function getWeather() {
     const city = document.getElementById('city').value;
-    const API_KEY = 'your_api_key'; // Replace with your OpenWeatherMap API key
+    const API_KEY = '67d5efe3b67b447caff7f0c61090aba7'; 
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
 
     const response = await fetch(url);
